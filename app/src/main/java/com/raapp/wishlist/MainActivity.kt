0c5a93e062b21557a6e1338c08351f1ebcb38219
Oleksii <rudenko2911@gmail.com>
2018-12-25 14:25:33 +0200

    migrate to androidX
    create EditActivity

--- app/src/main/java/com/raapp/wishlist/MainActivity.kt
+++ app/src/main/java/com/raapp/wishlist/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.raapp.wishlist
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
 
@@ -16,8 +17,9 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            startActivity(Intent(this, WishEditActivity::class.java))
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
         }
     }
 
@@ -32,7 +34,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
---
+++ app/src/main/java/com/raapp/wishlist/WishEditActivity.kt
@@ -0,0 +1,12 @@
+package com.raapp.wishlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WishEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wish_edit)
+    }
+}
