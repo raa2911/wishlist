6d90af47e3562395d166369cd6c22c11313c7445
Oleksii <rudenko2911@gmail.com>
2019-01-08 11:13:26 +0200

    add validation for title and link

--- app/src/main/java/com/raapp/wishlist/models/Wish.kt
+++ app/src/main/java/com/raapp/wishlist/models/Wish.kt
@@ -3,16 +3,15 @@ package com.raapp.wishlist.models
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.raapp.wishlist.Constants.EMPTY_STRING
+import java.io.Serializable
 
 @Entity(tableName = "wish")
 data class Wish(
     var title: String = EMPTY_STRING,
-//    @TypeConverters(PrivacyType::class)
-//    var privacy: PrivacyType = PrivacyType.PRIVATE,
     var privacy: Int = PrivacyType.PRIVATE.ordinal,
     var link: String? = null,
     var description: String? = null
-) {
+) : Serializable {
     @PrimaryKey(autoGenerate = true)
     var uid: Int = 0
 }
@@ -24,18 +23,6 @@ enum class PrivacyType {
         fun getById(id: Int): PrivacyType {
             return if (id < 0 || id > 2) PrivacyType.PRIVATE else PrivacyType.values()[id]
         }
-
-//        @TypeConverter
-//        @JvmStatic
-//        fun getPrivacyType(id: Int): PrivacyType {
-//            return getById(id)
-//        }
-//
-//        @TypeConverter
-//        @JvmStatic
-//        fun getPrivacyTypeInt(type: PrivacyType): Int {
-//            return type.ordinal
-//        }
     }
 }
 
---
+++ app/src/main/java/com/raapp/wishlist/utils/SimpleTextWatcher.kt
@@ -0,0 +1,14 @@
+package com.raapp.wishlist.utils
+
+import android.text.Editable
+import android.text.TextWatcher
+
+class SimpleTextWatcher(val callback: (text: CharSequence?) -> Unit) : TextWatcher {
+    override fun afterTextChanged(s: Editable?) {}
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        callback.invoke(s)
+    }
+}
